47f28c78fedf0fb252a27bfe65564acdbcceaad9
Bantibhagat27 <bantibhagatcsbs27.in@gmail.com>
2025-05-22 11:55:34 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+// app.js
+const express = require('express');
+const cors = require('cors');
+const userRoutes = require('./routes/userRoutes');
+const bookingRoutes = require('./routes/bookingRoutes');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/users', userRoutes);
+app.use('/api/bookings', bookingRoutes);
+
+module.exports = app;
